1ca6c6992da4cf0d6198c8e1d134ab5de18dd17f
GruzdevAV <102538170+GruzdevAV@users.noreply.github.com>
2024-02-11 21:53:55 +0300

    Изменил установку стилей. Добавил поддержку формул.

---
+++ DiplomMaker/CSharpCode/IgnoreCaseComparer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DiplomMaker
+{
+    /// <summary>
+    /// Сравнивает строки, игнорируя регистр.
+    /// Технически считает, что все строки в верхнем регистре.
+    /// </summary>
+    public class IgnoreCaseComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            return x.ToUpper().Equals(y.ToUpper());
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return obj.ToUpper().GetHashCode();
+        }
+    }
+}
